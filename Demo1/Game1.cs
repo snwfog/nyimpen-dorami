936b81915b52f941c43463cc34198f2e05984fab
snwfog <donchoa@gmail.com>
2013-10-24 21:54:45 -0400

    Add 10 dora's

--- Demo1/Game1.cs
+++ Demo1/Game1.cs
@@ -21,7 +21,7 @@ namespace Demo1
     GraphicsDeviceManager graphics;
     SpriteBatch spriteBatch;
 
-    private int maxNumberOfBadGuysTM = 4;
+    private int maxNumberOfBadGuysTM = 10;
     private List<SatsuiNoHadoDoraemon> badGuysTM; // TM for Trademark lol
     private int maxNumberOfAirGuns = 2;
     private List<AirGun> ammoRack;
